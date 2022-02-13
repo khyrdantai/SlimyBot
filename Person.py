74ea3c63d034846f5fcc562681780e43e4a30310
Harry James Hocker <harry_hocker@icloud.com>
2022-02-13 14:59:12 -0500

    Removed comment.

--- Person.py
+++ Person.py
@@ -2,7 +2,6 @@ class Person:
     birth_date = 0
     discord_id = 0
 
-    # a constructor for the object
     def __init__(self, birth_date, discord_id):
         self.birth_date = birth_date
         self.discord_id = discord_id
